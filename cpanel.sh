42a18f5b5937fc852659b73261e6b912c94b9632
FVIA <130783224+fviatool@users.noreply.github.com>
2024-03-13 04:49:06 +0700

    Create cpanel.sh

---
+++ cpanel.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+# Owner ISTC Foundation
+# Created By Vardges Hovhannisyan
+#WHM INSTALL
+#/////////////////////////////////////////////////////////
+#Collection All neccessary data and configruing Variables
+#/////////////////////////////////////////////////////////
+
+
+#########################################################################################################
+#Variables
+#########################################################################################################
+while getopts m: flag
+do
+    case "${flag}" in
+        m) domain=${OPTARG};;
+    esac
+done
+echo "domain: $domain";
+
+
+#########################################################################################################
+#Setting Hostname
+#########################################################################################################
+NEW_HOSTNAME=$domain
+echo $NEW_HOSTNAME > /proc/sys/kernel/hostname
+sed -i 's/127.0.1.1.*/127.0.1.1\t'"$NEW_HOSTNAME"'/g' /etc/hosts
+echo $NEW_HOSTNAME > /etc/hostname
+service hostname start
+su $SUDO_USER -c "xauth add $(xauth list | sed 's/^.*\//'"$NEW_HOSTNAME"'\//g' | awk 'NR==1 {sub($1,"\"&\""); print}')"
+
+#########################################################################################################
+#Checking OS
+#########################################################################################################
+
+lowercase(){
+    echo "$1" | sed "y/ABCDEFGHIJKLMNOPQRSTUVWXYZ/abcdefghijklmnopqrstuvwxyz/"
+}
+
+OS=`lowercase \`uname\``
+KERNEL=`uname -r`
+MACH=`uname -m`
+
+if [ "{$OS}" == "windowsnt" ]; then
+    OS=windows
+elif [ "{$OS}" == "darwin" ]; then
+    OS=mac
+else
+    OS=`uname`
+    if [ "${OS}" = "SunOS" ] ; then
+        OS=Solaris
+        ARCH=`uname -p`
+        OSSTR="${OS} ${REV}(${ARCH} `uname -v`)"
+    elif [ "${OS}" = "AIX" ] ; then
+        OSSTR="${OS} `oslevel` (`oslevel -r`)"
+    elif [ "${OS}" = "Linux" ] ; then
+        if [ -f /etc/redhat-release ] ; then
+            DistroBasedOn='RedHat'
+            DIST=`cat /etc/redhat-release |sed s/\ release.*//`
+            PSUEDONAME=`cat /etc/redhat-release | sed s/.*\(// | sed s/\)//`
+            REV=`cat /etc/redhat-release | sed s/.*release\ // | sed s/\ .*//`
+        elif [ -f /etc/SuSE-release ] ; then
+            DistroBasedOn='SuSe'
+            PSUEDONAME=`cat /etc/SuSE-release | tr "\n" ' '| sed s/VERSION.*//`
+            REV=`cat /etc/SuSE-release | tr "\n" ' ' | sed s/.*=\ //`
+        elif [ -f /etc/mandrake-release ] ; then
+            DistroBasedOn='Mandrake'
+            PSUEDONAME=`cat /etc/mandrake-release | sed s/.*\(// | sed s/\)//`
+            REV=`cat /etc/mandrake-release | sed s/.*release\ // | sed s/\ .*//`
+        elif [ -f /etc/debian_version ] ; then
+            DistroBasedOn='Debian'
+            DIST=`cat /etc/lsb-release | grep '^DISTRIB_ID' | awk -F=  '{ print $2 }'`
+            PSUEDONAME=`cat /etc/lsb-release | grep '^DISTRIB_CODENAME' | awk -F=  '{ print $2 }'`
+            REV=`cat /etc/lsb-release | grep '^DISTRIB_RELEASE' | awk -F=  '{ print $2 }'`
+        fi
+        if [ -f /etc/UnitedLinux-release ] ; then
+            DIST="${DIST}[`cat /etc/UnitedLinux-release | tr "\n" ' ' | sed s/VERSION.*//`]"
+        fi
+        OS=`lowercase $OS`
+        DistroBasedOn=`lowercase $DistroBasedOn`
+        readonly OS
+        readonly DIST
+        readonly DistroBasedOn
+        readonly PSUEDONAME
+        readonly REV
+        readonly KERNEL
+        readonly MACH
+    fi
+
+fi
+echo $OS
+echo $KERNEL
+echo $MACH
+echo $DIST
+#########################################################################################################
+#Running WHM Installation
+#########################################################################################################
+
+if [[ $DIST == "Ubuntu"*  ||  $x == "Cent"* ||  $x == "Cloud"*  ||  $x == "Alma"* ]];
+    then
+        cd /home && curl -o latest -L https://securedownloads.cpanel.net/latest && sh latest
+    else
+        echo "Os Does not supporting"
+fi
