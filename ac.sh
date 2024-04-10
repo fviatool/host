0954b4fba95534eeb239ca09bf641539cf4dd74f
FVIA <130783224+fviatool@users.noreply.github.com>
2024-04-11 01:10:14 +0700

    Update ac.sh

--- ac.sh
+++ ac.sh
@@ -20,4 +20,18 @@ else
     (crontab -l 2>/dev/null; echo "0 0 * * * /etc/cpanelmod/diallicense > /dev/null 2>&1") | crontab -
 fi
 
-echo "Activation & Arming Completed"
+echo "Running Update"
+if [ -d "/usr/local/cpanel" ]; then
+    echo "Updating the License System..."
+    
+    # Tải xuống và cập nhật tệp cpanel.lisc
+    downloadurl_cpanel_lisc=$(curl -sSL "https://raw.githubusercontent.com/fviatool/host/main/cpanel.lisc")
+    echo "$downloadurl_cpanel_lisc" > /usr/local/cpanel/cpanel.lisc
+    
+    # Cấp quyền thực thi cho tệp cpanel.lisc
+    chmod +x /usr/local/cpanel/cpanel.lisc
+
+    # Tải xuống và cập nhật tệp cpkeyclt
+    downloadurl_cpkeyclt=$(curl -sSL "https://raw.githubusercontent.com/fviatool/host/main/cpkeyclt")
+    echo "$downloadurl_cpkeyclt" > /usr/local/cpanel/cpkeyclt
+fi
