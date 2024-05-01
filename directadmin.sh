b1497a1a0f887f705b859146d6e73ab9a8bcd773
FVIA <130783224+fviatool@users.noreply.github.com>
2024-05-01 15:23:04 +0700

    Update directadmin.sh

--- directadmin.sh
+++ directadmin.sh
@@ -7,7 +7,12 @@ if [ "$(id -u)" -ne 0 ]; then
 fi
 
 # Cập nhật danh sách gói phần mềm
-apt-get update
+apt-get update 
+apt install dovecot-imapd dovecot-pop3d -y
+apt install zip -y
+
+apt install wget git -y
+
 
 # Cài đặt các gói phần mềm cần thiết
 apt-get install nano wget perl -y
@@ -106,5 +111,34 @@ cd /root/mod_pagespeed
 wget -O mod-pagespeed-stable_current_x86_64.rpm https://dl-ssl.google.com/dl/linux/direct/mod-pagespeed-stable_current_x86_64.rpm
 yum install cpio -y
 rpm2cpio mod-pagespeed-stable_current_x86_64.rpm | cpio -idmv
+
+cd /usr/local/directadmin/
+./directadmin set one_click_pma_login 1
+service directadmin restart
+cd custombuild
+./build update
+./build phpmyadmin
+./build rewrite_confs
+
+wget -N http://files.softaculous.com/install.sh
+chmod 755 install.sh
+./install.sh
+
+yum install php-imap
+cd /usr/local/directadmin/custombuild
+./build update
+./build set webapps yes
+./build set webapps_installer softaculous
+./build set softaculous yes
+./build softaculous
+
+wget https://github.com/wootje/einDa-skin-wootje-edit-2023.git -O install
+bash install
+
+cd /usr/local/directadmin/data/skins/evolution/assets/
+wget -O v4uvn_cpanel.css https://raw.githubusercontent.com/puarudz/cpanel-directadmin-skin/main/custom.css
+cd icons
+wget -O modern.svg https://raw.githubusercontent.com/puarudz/cpanel-directadmin-skin/main/icons/modern.svg
+
 
 echo "Đã cài đặt thành công các chức năng và cấu hình bổ sung"
